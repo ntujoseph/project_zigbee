5c28ac3ba58b39d2eceb54da555474b7e0808042
itrobotics <joseph@ittraining.com.tw>
2016-10-15 11:25:14 +0800

    modify main.c using snprintf

--- sample_code/RF_Relay Sample_v1.0/Listings/main.c
+++ sample_code/RF_Relay Sample_v1.0/Listings/main.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include "autonet.h"
 
+#define PRINT_BUFSIZE 64
+
 int main(void)
 {
 	uint8_t rcvd_msg[20];
@@ -11,8 +13,8 @@ int main(void)
 	uint8_t rcvd_length;
 	uint8_t rcvd_payloadLength;
 	uint8_t rcvd_rssi;
-	uint8_t output_array[10];
-
+	uint8_t output_array[PRINT_BUFSIZE];
+  uint8_t len;
 	uint8_t Type;
 	uint16_t Addr;
 	uint8_t radio_channel;
@@ -22,8 +24,13 @@ int main(void)
 	Addr = 0x0003;
 	radio_channel = 25;
 	radio_panID = 0x0007;
-
+  
+ 
+	
 	Initial(Addr,Type, radio_channel, radio_panID);
+	
+	len=snprintf((char *)output_array,PRINT_BUFSIZE,"starging Zigbee Relay......\r\n");
+	COM2_Tx(output_array,len);
 
 	while(1){
 		if(RF_Rx(rcvd_msg, &rcvd_length, &rcvd_rssi)){
